a00b3501dc70e7d76a0e0c4d93c4594a411def46
Onyekachukwu Ejiofor Nweke <nwekeejioforscheller@gmail.com>
2022-10-14 22:27:38 +0000

    [fix]: Task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
 # install flask from pip3
 
 exec { 'install python packages':
-  command => '/usr/bin/pip3 install flask -v 2.1.0',
+  command => 'pip3 install flask==2.1.0 flask_restful apiai',
+  path => ['/usr/bin/']
 }
